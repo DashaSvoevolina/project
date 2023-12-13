7bf4985b107d11c5e953324121f63afad7a643d4
Dasha <dsvoevollina06@gmail.com>
2023-12-13 13:34:32 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict"; // (говорим файлу, что работаем в современном, строгом режиме)
+//let number = 5; // (let-мы можем поменять значение переменой) 
+//const LeftBorderWidth = 1; // (const- значение переменой постоянное) 
+
+//number = 10;
+//console.log(number);
+
+//урок 11
+//const obj = {name:'John', age: 25, isMarried: false};
+//console.log(obj.age);
+
+//урок 13
+const answers = [];
+answers[0]= prompt( 'Как ваше имя?', '');
+answers[1]= prompt( 'Как ваша фамилия?', '');
+answers[2]= prompt( 'Сколько вам лет?', '');
+document.write(answers);
