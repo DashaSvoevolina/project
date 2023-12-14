5d928cd4efd8ab9d4f0fc3b5c7a18976e9876df5
Dasha <dsvoevollina06@gmail.com>
2023-12-14 12:38:24 +0600

    опросник о фильмах

--- js/script.js
+++ js/script.js
@@ -15,3 +15,20 @@
 //answers[1]= prompt( 'Как ваша фамилия?', '');
 //answers[2]= prompt( 'Сколько вам лет?', '');
 //document.write(answers);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+const personalMovieDB = {
+    count:numberOfFilms,
+    movies: {},
+    actors: {},
+    geners: [],
+    privat: false
+};
+
+const a= prompt('Один из последних просмотренных фильмов?',''),
+      b= prompt('На сколько оцените его?', ''),
+      c= prompt('Один из последних просмотренных фильмов?',''),
+      d= prompt('На сколько оцените его?', '');
+     
+personalMovieDB.movies[a]= b;
+personalMovieDB.movies[c]= d;
+console.log(personalMovieDB);
